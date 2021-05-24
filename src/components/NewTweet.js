437f5a2c73d09e3b6360d9f1f96a32af6e2ab3ec
sandeep2194 <sandeep2194@live.com>
2021-05-24 18:28:55 +0530

    added react router and implemented  it

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { handleAddTweet } from '../actions/tweets'
+import { Redirect } from 'react-router-dom'
 
 class NewTweet extends Component {
     state = {
         text: '',
+        toHome: false,
     }
     handleChange = (e) => {
         const text = e.target.value
@@ -23,12 +25,17 @@ class NewTweet extends Component {
 
         this.setState(() => ({
             text: '',
+            toHome: id ? false : true,
         }))
     }
     render() {
-        const { text } = this.state
+        const { text, toHome } = this.state
         const tweetLeft = 280 - text.length
-        {/*  todo: Redirect to home view if submitted */ }
+
+        if (toHome === true) {
+            return <Redirect to='/' />
+        }
+
         return (
             <div>
                 <h3 className='center'>
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -5,6 +5,7 @@ import { TiArrowBackOutline } from 'react-icons/ti/index'
 import { TiHeartOutline } from 'react-icons/ti/index'
 import { TiHeartFullOutline } from 'react-icons/ti/index'
 import {handleToggleTweet} from '../actions/tweets'
+import {Link, withRouter} from 'react-router-dom'
 
 class Tweet extends Component {
     handleLike = (e) =>{
@@ -19,7 +20,7 @@ class Tweet extends Component {
     }
     toParent = (e, id) => {
         e.preventDefault()
-        //todo: Redirect to the parent tweet.
+        this.props.history.push(`/tweet/${id}`)
     }
     render() {
         const {tweet} = this.props
@@ -29,10 +30,10 @@ class Tweet extends Component {
         }
 
         const {
-            name, avatar, timestamp, text, hasLiked, likes, replies, parent
+            name, avatar, timestamp, text, hasLiked, likes, replies, parent,id
         } = tweet
         return (
-            <div className='tweet'>
+            <Link to={`/tweet/${id}`} className='tweet'>
                 <img 
                 src={avatar}
                 alt={`Avatar of ${name}`}
@@ -61,7 +62,7 @@ class Tweet extends Component {
                     <span>{likes!==0 && likes}</span>
                     </div>
                 </div>
-            </div>
+            </Link>
         )
     }
 }
@@ -75,4 +76,4 @@ function mapStateToProps({ authedUser, users, tweets }, { id }) {
     }
 }
 
-export default connect(mapStateToProps)(Tweet)
+export default withRouter(connect(mapStateToProps)(Tweet))
